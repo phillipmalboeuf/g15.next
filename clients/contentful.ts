c609965fe6478b55d056d1be2ba835d8ba9e42f2
Phillip Malboeuf <phil@phils.computer>
2023-09-06 12:32:22 -0400

    preview contentful

--- clients/contentful.ts
+++ clients/contentful.ts
@@ -1,6 +1,9 @@
 import { createClient } from 'contentful'
 
+const preview = true
+
 export const contentful = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
-  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+  accessToken: preview ? process.env.CONTENTFUL_PREVIEW_TOKEN : process.env.CONTENTFUL_ACCESS_TOKEN,
+  host: preview ? "preview.contentful.com" : undefined
 }).withoutUnresolvableLinks
