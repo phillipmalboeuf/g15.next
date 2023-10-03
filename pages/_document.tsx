305292e52fffd867713a475b993e2264f51abe80
Phillip Malboeuf <phil@phils.computer>
2023-10-03 09:52:20 -0400

    simpleanalytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,9 @@ export default function Document() {
       </Head>
       <body>
         <Main />
+
+        <script async defer src="https://scripts.simpleanalyticscdn.com/latest.js"></script> 
+        <noscript><img src="https://queue.simpleanalyticscdn.com/noscript.gif" alt="" referrerPolicy="no-referrer-when-downgrade" /></noscript>
         <NextScript />
       </body>
     </Html>
